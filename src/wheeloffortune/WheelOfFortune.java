c647820a9c3cac1530b691b670f98f7023cea7da
Phil O'Connell <pxo4@psu.edu>
2016-09-11 22:37:36 -0400

    Initial code.

---
+++ src/wheeloffortune/WheelOfFortune.java
@@ -0,0 +1,20 @@
+/*
+* CMPSC 261, Section 1
+* Fall 2016
+* Instructor: Phil O'Connell
+* TODO: DON'T FORGET TO PUT YOUR NAME AND ID
+* Student: Stew Dent
+* ID: abc1234
+*/
+package wheeloffortune;
+
+public class WheelOfFortune {
+
+  /**
+   * @param args the command line arguments
+   */
+  public static void main(String[] args) {
+    
+  }
+  
+}
