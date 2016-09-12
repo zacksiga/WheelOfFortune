f84d6df4da1412fa522d1b384cea770c3ceab471
pxo4 <pxo4@OZWDLND132-194.win.psu.edu>
2016-09-12 08:25:26 -0400

    Updated the student info

--- src/wheeloffortune/WheelOfFortune.java
+++ src/wheeloffortune/WheelOfFortune.java
@@ -2,9 +2,8 @@
 * CMPSC 261, Section 1
 * Fall 2016
 * Instructor: Phil O'Connell
-* TODO: DON'T FORGET TO PUT YOUR NAME AND ID
-* Student: Stew Dent
-* ID: abc1234
+* Student: Phil O'Connell 
+* ID: pxo4
 */
 package wheeloffortune;
 
