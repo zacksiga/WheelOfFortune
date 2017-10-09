9b3357d52dbc963e6e1cd23ea9bb624620bc4470
zvs5039 <zvs5039@OZWDLND132-219.win.psu.edu>
2017-10-09 08:48:59 -0400

    Added "hello people" print line to main method.

--- src/wheeloffortune/WheelOfFortune.java
+++ src/wheeloffortune/WheelOfFortune.java
@@ -2,8 +2,8 @@
 * CMPSC 261, Section 1
 * Fall 2016
 * Instructor: Phil O'Connell
-* Student: Phil O'Connell 
-* ID: pxo4
+* Student: Zachary Sigamony 
+* ID: zvs5039
 */
 package wheeloffortune;
 
@@ -13,6 +13,7 @@ public class WheelOfFortune {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
+      System.out.println("Hello people");
     
   }
   
